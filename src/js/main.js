b3dea8129bdaa15a364dd17107b8dfe1bacf3f92
monnard-guillaume <monnard.guillaume@gmail.com>
2025-12-11 14:03:58 +0100

    reset du curseur et de l'image

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,3 @@
-// const container = document.querySelector(".artists-container");
-// const thumb = document.querySelector(".custom-thumb");
-
-// function updateThumb() {
-//   const scrollHeight = container.scrollHeight - container.clientHeight;
-//   const scrollTop = container.scrollTop;
-//   const maxThumbTop = container.clientHeight - thumb.offsetHeight;
-//   const thumbTop = (scrollTop / scrollHeight) * maxThumbTop;
-//   thumb.style.top = `${thumbTop}px`;
-// }
-
-// // Mise à jour au scroll
-// container.addEventListener("scroll", updateThumb);
-// // Mise à jour au load et resize
-// window.addEventListener("load", updateThumb);
-// window.addEventListener("resize", updateThumb);
-
 const container = document.querySelector(".artists-container");
 
 let currentScroll = container.scrollTop;
@@ -48,55 +31,3 @@ function smoothScroll() {
 }
 
 smoothScroll();
-
-// Dictionnaire des images de curseur par artiste
-const artistCursors = {
-  Gal: "assets/img/cursor/gal.png",
-  "Jeune Mort": "assets/img/cursor/jeune-mort.png",
-  "M le maudit": "assets/img/cursor/m-le-maudit.png",
-  Sheldon: "assets/img/cursor/sheldon.png",
-  Shien: "assets/img/cursor/shien.png",
-  Sopico: "assets/img/cursor/sopico.png",
-  "White Corbeau": "assets/img/cursor/white-corbeau.png",
-  "Yung Coeur": "assets/img/cursor/yung-coeur.png",
-  Zinee: "assets/img/cursor/zinee.png",
-};
-
-// Curseur par défaut
-const defaultCursor = "assets/cursor/red-cursor.svg";
-
-// Création du curseur custom
-const customCursor = document.createElement("div");
-customCursor.style.position = "fixed";
-customCursor.style.width = "50px";
-customCursor.style.height = "50px";
-customCursor.style.pointerEvents = "none";
-customCursor.style.background = `url(${defaultCursor}) no-repeat center center`;
-customCursor.style.backgroundSize = "contain";
-customCursor.style.zIndex = "9999";
-customCursor.style.transform = "translate(-50%, -50%)";
-customCursor.style.transition = "background-image 0.2s ease";
-document.body.appendChild(customCursor);
-
-// Suivi de la souris
-document.addEventListener("mousemove", (e) => {
-  customCursor.style.left = `${e.clientX}px`;
-  customCursor.style.top = `${e.clientY}px`;
-});
-
-// Gestion du hover sur les artistes
-document.querySelectorAll(".artist-link").forEach((link) => {
-  const artistName = link.querySelector(".artist-name").textContent.trim();
-
-  link.addEventListener("mouseenter", () => {
-    if (artistCursors[artistName]) {
-      customCursor.style.background = `url(${artistCursors[artistName]}) no-repeat center center`;
-      customCursor.style.backgroundSize = "contain";
-    }
-  });
-
-  link.addEventListener("mouseleave", () => {
-    customCursor.style.background = `url(${defaultCursor}) no-repeat center center`;
-    customCursor.style.backgroundSize = "contain";
-  });
-});
