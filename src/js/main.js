2eae4bb7b5e6ef0a99f406b0733e10e9fa9ef1b8
monnard-guillaume <monnard.guillaume@gmail.com>
2025-12-15 12:58:03 +0100

    bg-hover au premier plan

--- src/js/main.js
+++ src/js/main.js
@@ -63,15 +63,31 @@ artists.forEach((link) => {
   if (!name || !map[name]) return;
 
   link.addEventListener("mouseenter", () => {
-    document.body.style.backgroundImage = `url("${map[name]}")`;
-    document.body.style.backgroundSize = "auto";
-    document.body.style.backgroundPosition = "right center";
-    document.body.style.backgroundRepeat = "no-repeat";
+    // Mettre l'image dans bg-hover
+    bgHover.style.backgroundImage = `url("${map[name]}")`;
     bgHover.style.opacity = "1";
   });
 
   link.addEventListener("mouseleave", () => {
-    document.body.style.backgroundImage = "";
     bgHover.style.opacity = "0";
   });
 });
+
+// artists.forEach((link) => {
+//   const nameEl = link.querySelector(".artist-name");
+//   const name = nameEl ? nameEl.textContent.trim() : null;
+//   if (!name || !map[name]) return;
+
+//   link.addEventListener("mouseenter", () => {
+//     document.body.style.backgroundImage = `url("${map[name]}")`;
+//     document.body.style.backgroundSize = "auto";
+//     document.body.style.backgroundPosition = "right center";
+//     document.body.style.backgroundRepeat = "no-repeat";
+//     bgHover.style.opacity = "1";
+//   });
+
+//   link.addEventListener("mouseleave", () => {
+//     document.body.style.backgroundImage = "";
+//     bgHover.style.opacity = "0";
+//   });
+// });
